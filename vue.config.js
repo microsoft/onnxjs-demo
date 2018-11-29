79439fc524ecf27b6bc012f5763108fa08f752d0
Hari Seshadri <hasesh@microsoft.com>
2018-11-28 16:25:09 -0800

    Demo trial iter #10

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
       }
     }
   },
-  baseUrl: process.env.NODE_ENV === 'production'? '/': '/',
+  baseUrl: process.env.NODE_ENV === 'production'? '/onnxjs-demo/': '/',
   outputDir: 'docs',
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
